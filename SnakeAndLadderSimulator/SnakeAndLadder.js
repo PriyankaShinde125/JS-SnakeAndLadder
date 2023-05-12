6444348e5ef807d2e450ec5dad5ccc4fd1e706d9
PriyankaShinde125 <pkshinde125@gmail.com>
2023-05-12 15:39:01 +0530

    ADDED : Code to display play no and position

--- SnakeAndLadderSimulator/SnakeAndLadder.js
+++ SnakeAndLadderSimulator/SnakeAndLadder.js
@@ -5,7 +5,7 @@ const LADDER = 1;
 const WINNING_POSITION = 100;
 
 let currentPosition = 0;
-console.log("Start position :" + START_POSITION);
+//console.log("Start position :" + START_POSITION);
 
 let positionArray = [];
 
@@ -13,16 +13,16 @@ let positionArray = [];
 while (currentPosition < WINNING_POSITION) {
   play();
 }
-console.log("Current Position after move : " + currentPosition);
+//console.log("Current Position after move : " + currentPosition);
 
 function play() {
   //Generate random die number
   let dieNumber = Math.floor(Math.random() * 6 + 1);
-  console.log("Die generated : " + dieNumber);
+  //console.log("Die generated : " + dieNumber);
 
   //Decide player move
   let move = Math.floor(Math.random() * 2);
-  console.log("Move decider :" + move);
+  // console.log("Move decider :" + move);
 
   //Update current position
   switch (move) {
@@ -41,4 +41,9 @@ function play() {
   }
 
   positionArray.push(currentPosition);
+
+  console.log("No of play \t Position at that play");
+  for (let index = 0; index < positionArray.length; index++) {
+    console.log(index + 1 + "\t" + positionArray[index]);
+  }
 }
