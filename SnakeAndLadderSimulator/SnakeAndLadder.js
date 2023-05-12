577d932495bd0e9477f6fe41a48317ca7edc2fbe
PriyankaShinde125 <pkshinde125@gmail.com>
2023-05-12 12:46:31 +0530

    ADDED : Code to generate random die number

--- SnakeAndLadderSimulator/SnakeAndLadder.js
+++ SnakeAndLadderSimulator/SnakeAndLadder.js
@@ -1,2 +1,7 @@
+//Initialized with start position 0
 const startPosition=0
 console.log(startPosition)
+
+//Generate random die number
+let dieNumber = Math.floor((Math.random() * 6) + 1)
+console.log(dieNumber)
