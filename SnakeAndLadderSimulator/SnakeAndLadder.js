391bc0785b98d99483d1e216e49d2156df2992bd
PriyankaShinde125 <pkshinde125@gmail.com>
2023-05-12 15:05:40 +0530

    ADDED : Code to play player until win

--- SnakeAndLadderSimulator/SnakeAndLadder.js
+++ SnakeAndLadderSimulator/SnakeAndLadder.js
@@ -1,28 +1,33 @@
 //Initialized with start position 0
 const START_POSITION = 0;
-let currentPosition = 0;
 const SNAKE = 0;
 const LADDER = 1;
+const WINNING_POSITION = 100;
+
+let currentPosition = 0;
 console.log("Start position :" + START_POSITION);
 
-//Generate random die number
-let dieNumber = Math.floor(Math.random() * 6 + 1);
-console.log("Die generated : " + dieNumber);
+//Play until win
+while (currentPosition <= WINNING_POSITION) {
+  //Generate random die number
+  let dieNumber = Math.floor(Math.random() * 6 + 1);
+  console.log("Die generated : " + dieNumber);
 
-//Decide player move
-let move = Math.floor(Math.random() * 2);
-console.log("Move decider :" + move);
+  //Decide player move
+  let move = Math.floor(Math.random() * 2);
+  console.log("Move decider :" + move);
 
-//Update current position
-switch (move) {
-  case LADDER:
-    currentPosition = currentPosition + dieNumber;
-    break;
-  case SNAKE:
-    if (currentPosition - dieNumber < 0) currentPosition = START_POSITION;
-    else currentPosition = currentPosition - dieNumber;
-    break;
-  default:
-    break;
+  //Update current position
+  switch (move) {
+    case LADDER:
+      currentPosition = currentPosition + dieNumber;
+      break;
+    case SNAKE:
+      if (currentPosition - dieNumber < 0) currentPosition = START_POSITION;
+      else currentPosition = currentPosition - dieNumber;
+      break;
+    default:
+      break;
+  }
 }
 console.log("Current Position after move : " + currentPosition);
